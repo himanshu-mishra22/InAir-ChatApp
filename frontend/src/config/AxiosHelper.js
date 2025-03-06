e4461f3fb6eea16cfd70e15a00adc73aabd139a9
HIMANSHU PRAKASH MISHRA <mhimanshuprakash@gmail.com>
2025-03-06 10:35:06 +0530

    updated

--- frontend/src/config/AxiosHelper.js
+++ frontend/src/config/AxiosHelper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://chatbackend-nkh3.onrender.com";
 export const httpClient = axios.create({
     baseURL: API_URL,
     
